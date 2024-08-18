cb3ef0c4be24f3bbd095d1ca0aeb7796696964c4
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-17 20:49:13 -0400

    Implemented argument validation and error handling for bmiCalculator

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface BmiValues {
+  height: number;
+  mass: number;
+}
+
+const parseArguments = (args: string[]): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      mass: Number(args[3]),
+    };
+  } else {
+    throw new Error('Provided values were not numbers');
+  }
+};
+
 const calculateBmi = (height: number, mass: number): string => {
   const bmi = mass / (height / 100) ** 2;
   if (bmi < 18.5) {
@@ -11,7 +30,16 @@ const calculateBmi = (height: number, mass: number): string => {
   }
 };
 
+try {
+  const { height, mass } = parseArguments(process.argv);
+  console.log(calculateBmi(height, mass));
+} catch (error: unknown) {
+  let errorMessage = 'Something bad happened.';
+  if (error instanceof Error) {
+    errorMessage += ' Error: ' + error.message;
+  }
+  console.log(errorMessage);
+}
+
 const height: number = Number(process.argv[2]);
 const mass: number = Number(process.argv[3]);
-
-console.log(calculateBmi(height, mass));
