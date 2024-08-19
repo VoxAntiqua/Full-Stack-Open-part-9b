4cb8c3b792ab640cd897dc41280932a64908ac17
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-19 15:47:11 -0400

    Implemented error handling for bmi endpoint

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import { calculateBmi } from './bmiCalculator';
 const app = express();
 
@@ -6,12 +6,28 @@ app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req, res) => {
-  const height = req.query.height;
-  const weight = req.query.weight;
-  const result = calculateBmi(Number(height), Number(weight));
+app.get('/bmi', (req: Request, res: Response) => {
+  if (!req.query.height || !req.query.weight) {
+    res
+      .status(400)
+      .json({ error: 'Missing parameters: height and weight are required' });
+  }
 
-  res.send({ ...req.query, bmi: result });
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
+
+  if (isNaN(height) || isNaN(weight)) {
+    res.status(400).json({
+      error: 'Malformatted parameters: height and weight must be numbers',
+    });
+  }
+
+  try {
+    const result = calculateBmi(Number(height), Number(weight));
+    res.send({ ...req.query, bmi: result });
+  } catch (error) {
+    res.status(500).json({ error: 'An unexpected error occurred' });
+  }
 });
 
 const PORT = 3003;
