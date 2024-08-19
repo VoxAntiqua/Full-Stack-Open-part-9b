9f5bfc83494718b327e6958f9cf96378ccefafe5
VoxAntiqua <andrew.padgett.88@gmail.com>
2024-08-19 16:47:56 -0400

    implemented parameter check for post to exercises endpoint

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from 'express';
 import { calculateBmi } from './bmiCalculator';
 const app = express();
+app.use(express.json());
 
 app.get('/hello', (_req, res) => {
   res.send('Hello Full Stack!');
@@ -30,7 +31,25 @@ app.get('/bmi', (req: Request, res: Response) => {
   }
 });
 
-app.post('/exercises', (req: Request, res: Response) => {});
+app.post('/exercises', (req: Request, res: Response) => {
+  if (!req.body.daily_exercises || !req.body.target) {
+    res.status(400).json({
+      error: 'Missing parameters: daily_exercises and target are required',
+    });
+  } else {
+    const dailyHours: number[] = req.body.daily_exercises.map((n: string) =>
+      Number(n)
+    );
+    const target: number = Number(req.body.target);
+
+    if (dailyHours.some((n) => isNaN(n)) || isNaN(target)) {
+      res.status(400).json({
+        error: 'Malformatted parameters: all parameters must be numbers',
+      });
+    }
+    res.send('Request okay');
+  }
+});
 
 const PORT = 3003;
 
